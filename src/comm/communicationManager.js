712faac343de446cc73fecf71dc5b237d7c649f7
lbshayna_gmail <shaynaban@gmail.com>
2020-01-02 09:45:39 +0200

    communication manager

--- src/comm/communicationManager.js
+++ src/comm/communicationManager.js
@@ -1,5 +1,5 @@
 import channels from '../usefulgarbage/channels.json'
-// import  {addLayer,updateFeatureAttributes}  from "../redux/actions/actions";
+import  {addLayer,updateFeatureAttributes}  from "../redux/actions/actions";
 // import {ADD_LAYER,UPDATE_FEATURE_ATTRIBUTES} from '../actions/actionsTypes';
 import {WSkubeMQ} from '../comm/WSkubeMQ.js'; 
 import {Base64} from '../convertors/base64';
@@ -29,15 +29,44 @@ export const onMessageRecived = (message) => {
     });
 
     switch (channelItem.reduxFunction) {
-        case "":
-            
-            break;
-    
+        case "update_attributes":
+            updateFeatureAttributes([{
+                "unit-id": 340,
+                "changes": [{
+                    "field-name": "Status",
+                    "value": "FAIL"
+                },
+                {
+                    "field-name": "Time",
+                    "value": "2019222"
+                }]
+            },
+            {
+                "unit-id": 580,
+                "changes": [{
+                    "field-name": "Status",
+                    "value": "FAIL"
+                },
+                {
+                    "field-name": "Time",
+                    "value": "2019222"
+                }]
+            }],"units","unit-id","unit-id","changes", "field-name","value");
+                // var data = action.data;
+                // var target = action.target;       
+                // var idTargetKey = action.idTargetKey;
+                // var idSourceKey = action.idSourceKey;
+                // var atrributeListKey = action.atrributeListKey;
+                // var attributeKey = action.attributeKey;
+                // var attributeValue = action.attributeValue;              
+            break;    
         default:
             break;
     }
 
 }
+
+
 
 export const onErrorRecived = (error) => {
 
