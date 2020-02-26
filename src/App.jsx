b4e674b0a87321deb997c070a312566833a5c0d3
lbshayna_gmail <shaynaban@gmail.com>
2020-02-26 10:11:00 +0200

    fetching info

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,27 @@ import tools from './usefulgarbage/tools.json';
 import channels from './usefulgarbage/channels.json'
 
 import VisibleMap from './containers/VisibleMap';
-import {loadChannels}  from './comm/communicationManager.js'
+import { loadChannels } from './comm/communicationManager.js';
+import { fetchData } from './comm/apiManager.js'
 
 
 
- class App extends Component{  
-  
+
+
+class App extends Component {
+
   constructor(props) {
+
     super(props);
 
-  
-   
+    fetchData({
+      url: "https://localhost:5001/",
+      functionName: "MetaData",
+      method: "Get"
+    });
   }
-  
-  render(){
+
+  render() {
 
     console.log("channels:" + channels[0]);
 
@@ -27,13 +34,13 @@ import {loadChannels}  from './comm/communicationManager.js'
     return (
       <div>
         {/* <div><ToolList tools={tools}></ToolList></div>             */}
-       <div>
-         <div><VisibleMap></VisibleMap></div>
-          
-       </div>
+        <div>
+          <div><VisibleMap></VisibleMap></div>
+
+        </div>
       </div>
     )
-  }  
+  }
 }
 
 export default App;
