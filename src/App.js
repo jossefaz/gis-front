3eeda1f0746cc8fe5bf428cbe0839066049eb7b7
yossefaz <yossefaz@protonmail.com>
2020-03-29 13:06:04 +0300

    add App.js to git

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import ToolList from './components/tool/ToolList';
+import tools from './usefulgarbage/tools.json';
+import channels from './usefulgarbage/channels.json'
+
+import VisibleMap from './containers/VisibleMap';
+import { fetchData } from './comm/apiManager.js'
+import LayerList from './components/layers/LayerList';
+import './general.css';
+
+class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+  // var layers = fetchData({
+  //     url: "https://localhost:5001/",
+  //     functionName: "MetaData",
+  //     method: "Get"
+  //   });
+    
+  //   console.log(layers);
+  }
+
+  render() {
+
+    console.log("channels:" + channels[0]);
+
+    console.log(tools);
+    return (
+      <div>
+        {/* <div><ToolList tools={tools}></ToolList></div>             */}
+        <div className="rowC">
+         <VisibleMap></VisibleMap>
+         <LayerList></LayerList>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
