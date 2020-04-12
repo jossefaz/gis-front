560e2143b90b88b90825264486eb442fb180d1ad
yossefaz <yossefaz@protonmail.com>
2020-04-12 15:52:11 +0300

    Call initTools Action from TopNav Container. Related to https://gitlab.com/gis_manti/gis_front/-/issues/24

--- src/containers/TopNav/index.js
+++ src/containers/TopNav/index.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import { InitTools } from "../../redux/actions/tools";
+class TopNav extends React.Component {
+  componentDidMount() {
+    LogIt(logLevel.INFO, "TopNav init");
+    this.props.InitTools(config.get("Tools"));
+  }
 
-const TopNav = (props) => (
-  <div className="ui top fixed menu ">
-    <div className="item align left">
-      <div className="ui icon input">
-        <input type="text" placeholder="...חיפוש" />
-        <i className="search link icon"></i>
+  render() {
+    return (
+      <div className="ui top fixed menu ">
+        <div className="item align left">
+          <div className="ui icon input">
+            <input type="text" placeholder="...חיפוש" />
+            <i className="search link icon"></i>
+          </div>
+        </div>
+        <a className="fixed item" onClick={() => this.props.onLayerMenuOpen()}>
+          Layers
+        </a>
       </div>
-    </div>
-    <a className="fixed item" onClick={() => props.onLayerMenuOpen()}>
-      Layers
-    </a>
-  </div>
-);
-
-export default TopNav;
+    );
+  }
+}
+export default connect(null, { InitTools })(TopNav);
