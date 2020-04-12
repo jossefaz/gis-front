beb6f00cc365e56151c8506962b2716be47deedd
yossefaz <yossefaz@protonmail.com>
2020-04-12 14:59:55 +0300

    move Map from component to containers

---
+++ src/containers/Map/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { InitMap } from "./func";
+import { connect } from "react-redux";
+import config from "react-global-configuration";
+import { logLevel, LogIt } from "../../utils/logs";
+import { InitLayers } from "../../redux/actions/layers";
+import "./style.css";
+class MapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.map = null;
+  }
+  componentDidMount() {
+    this.map = InitMap();
+    LogIt(logLevel.INFO, "Map init");
+    this.props.InitLayers(config.get("layers"));
+  }
+
+  componentDidUpdate() {
+    LogIt(logLevel.INFO, "Map Update");
+    Object.keys(this.props.mapLayers).map((lyrId) =>
+      this.map.addLayer(this.props.mapLayers[lyrId])
+    );
+  }
+
+  render() {
+    const { target } = config.get("MapConfig");
+    return <div id={target} className="map"></div>;
+  }
+}
+const mapStateToProps = (state) => {
+  return { mapLayers: state.mapLayers };
+};
+
+export default connect(mapStateToProps, { InitLayers })(MapComponent);
--- src/containers/archive/VisibleMap/VisibleMap.jsx
+++ src/containers/archive/VisibleMap/VisibleMap.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import MapComponent from '../../components/Map';
-import  {addLayer,}  from "../../redux/actions/actions";
-import  {selectUnits}  from "../../redux/selectors/mantiSystemsSelector";
+import { addLayer, } from "../../../redux/actions/actions";
+import { selectUnits } from "../../../redux/selectors/mantiSystemsSelector";
 
 import CircleStyle from 'ol/style/Circle';
 import Fill from 'ol/style/Fill';
@@ -10,41 +10,41 @@ import Style from 'ol/style/Style';
 
 
 
-const  styleFunction = function(feature, resolution){
-    var styleOL = new Style( {
-        image: new CircleStyle( {
+const styleFunction = function (feature, resolution) {
+    var styleOL = new Style({
+        image: new CircleStyle({
             radius: 10,
-            fill: new Fill( {
+            fill: new Fill({
                 color: 'green'
-            } )
-        } )
-    } );
-    var styleCPS = new Style( {
-        image: new CircleStyle( {
+            })
+        })
+    });
+    var styleCPS = new Style({
+        image: new CircleStyle({
             radius: 10,
-            fill: new Fill( {
+            fill: new Fill({
                 color: 'blue'
-            } )
-        } )
-    } );
+            })
+        })
+    });
 
-    var styleOFFL = new Style( {
-        image: new CircleStyle( {
+    var styleOFFL = new Style({
+        image: new CircleStyle({
             radius: 10,
-            fill: new Fill( {
+            fill: new Fill({
                 color: 'black'
-            } )
-        } )
-    } );
+            })
+        })
+    });
 
-    var styleFAIL = new Style( {
-        image: new CircleStyle( {
+    var styleFAIL = new Style({
+        image: new CircleStyle({
             radius: 10,
-            fill: new Fill( {
+            fill: new Fill({
                 color: 'red'
-            } )
-        } )
-    } );
+            })
+        })
+    });
 
     switch (feature.get('CSTAT')) {
         case 'OL':
@@ -54,41 +54,41 @@ const  styleFunction = function(feature, resolution){
         case 'FAIL':
             return [styleFAIL]
         default:
-            return [styleOFFL];              
-    } 
+            return [styleOFFL];
+    }
 }
 
-class VisibleMap extends React.Component {   
-    
+class VisibleMap extends React.Component {
+
     render() {
 
-        return (        
-            <MapComponent layers={this.props.layers}   
-             addLayer={this.props.addLayer} 
-             updatePublishedStatus = {this.props.updatePublishedStatus}
-             units={this.props.units}></MapComponent>                              
+        return (
+            <MapComponent layers={this.props.layers}
+                addLayer={this.props.addLayer}
+                updatePublishedStatus={this.props.updatePublishedStatus}
+                units={this.props.units}></MapComponent>
         )
     }
 }
 
-const mapStateToProps = state =>  {
+const mapStateToProps = state => {
     return {
         layers: state.mapLayers.layers,
-        units : selectUnits(state)
-    }    
+        units: selectUnits(state)
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    // dispatching actions returned by action creators
-    addLayer : (layer) => dispatch(addLayer(layer))
-  }
+    return {
+        // dispatching actions returned by action creators
+        addLayer: (layer) => dispatch(addLayer(layer))
+    }
 }
 
 export default connect(
-    mapStateToProps,   
+    mapStateToProps,
     mapDispatchToProps,
-    null 
+    null
 )(VisibleMap);
 
 
