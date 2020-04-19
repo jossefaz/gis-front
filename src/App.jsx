5b02fd78ce222f5fd7bce29d9695cbd908b48855
lbshayna_gmail <shaynaban@gmail.com>
2020-04-19 14:40:33 +0300

    saving project

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
-
-import ToolList from './components/tool/ToolList/ToolList.jsx';
-
-import tools from './usefulgarbage/tools.json';
-import VisibleMap from './containers/VisibleMap/VisibleMap.jsx';
-import VisibleLayerList from './containers/VisibleLayerList/VisibleLayerList.jsx';
-
-class App extends Component {
-
+import React from 'react';
+import Map from "./containers/Map";
+import TopNav from "./containers/TopNav";
+import SideNav from "./containers/SideNav";
+import config from "react-global-configuration";
+import { logLevel, LogIt } from "./utils/logs";
+import { InitTools } from "./redux/actions/tools";
+import { connect } from "react-redux";
+import { InitLayers } from "./redux/actions/layers";
+import { InitRasters } from "./redux/actions/raster";
+import Widget from './containers/Widget';
+class App extends React.Component {
+  componentDidMount() {
+    LogIt(logLevel.INFO, "App init");
+    this.props.InitLayers(config.get("layers"));
+    this.props.InitRasters();
+    this.props.InitTools(config.get("Widgets"));
+  }
   render() {
-        
     return (
-      <div>
-        {/* <div><ToolList tools={tools}></ToolList></div>             */}
-        <div>
-         <VisibleMap></VisibleMap>
-         <VisibleLayerList></VisibleLayerList>
-        </div>
-      </div>
+      <React.Fragment>
+        <SideNav>
+          <div className="ui grid">
+            <div className="row">
+              <TopNav onLayerMenuOpen={this.onLayerMenuOpen} />
+            </div>
+            <div className="row">
+              <Map />
+            </div>
+          </div>
+        </SideNav>
+        <Widget />
+      </React.Fragment>
 
-    )
+    );
   }
 }
+const mapStateToProps = (state) => {
+  return { Tools: state.Tools };
+};
+export default connect(mapStateToProps, { InitTools, InitLayers, InitRasters })(App);
 
-export default App;
+
