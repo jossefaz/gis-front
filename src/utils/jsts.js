1252db7043531f65bd371203a681da85b3c409cf
jossefaz <jossefaz@protonmail.com>
2020-12-09 21:25:13 +0200

    add buffer jsts ok

---
+++ src/utils/jsts.js
@@ -0,0 +1,30 @@
+import * as jsts from "jsts";
+import LinearRing from "ol/geom/LinearRing";
+import {
+  LineString,
+  MultiLineString,
+  MultiPoint,
+  MultiPolygon,
+  Point,
+  Polygon,
+} from "ol/geom";
+import { getCurrentProjection } from "../nessMapping/api";
+import { transformExtent } from "ol/proj";
+export const getBufferedFeature = (olFeature, bufferSize) => {
+  const geom = olFeature.getGeometry();
+  const parser = new jsts.io.OL3Parser();
+  parser.inject(
+    Point,
+    LineString,
+    LinearRing,
+    Polygon,
+    MultiPoint,
+    MultiLineString,
+    MultiPolygon
+  );
+  const jstsGeom = parser.read(geom);
+  const buffered = jstsGeom.buffer(bufferSize);
+  const finalGeom = parser.write(buffered);
+  olFeature.setGeometry(finalGeom);
+  return olFeature;
+};
