22fa8e5c332b0318ecc79cf34cf1b35948f74652
jossefaz <jossefaz@protonmail.com>
2021-03-30 12:10:03 +0300

    add control to map menu

--- src/components/MapMenu/index.tsx
+++ src/components/MapMenu/index.tsx
@@ -1,16 +1,7 @@
-import React from "react";
-import API from "../../core/api";
+import ZoomControl from "./ZoomControl";
+import XYControl from "./XYControl";
+
 const MapMenu: React.FC = () => {
-  const ZoomIn = (e: React.MouseEvent<HTMLElement>) => {
-    e.stopPropagation();
-    API.map.zoomIn();
-  };
-
-  const ZoomOut = (e: React.MouseEvent<HTMLElement>) => {
-    e.stopPropagation();
-    API.map.zoomOut();
-  };
-
   return (
     <div className="map-menu">
       <div className="map-menu__group">
@@ -29,14 +20,7 @@ const MapMenu: React.FC = () => {
           <i className="gis-icon gis-icon--location-on-pic icon-lg"></i>
         </div>
       </div>
-      <div className="map-menu__group">
-        <div className="map-menu__item map-menu__link" onClick={ZoomIn}>
-          <i className="gis-icon gis-icon--plus"></i>
-        </div>
-        <div className="map-menu__item map-menu__link" onClick={ZoomOut}>
-          <i className="gis-icon gis-icon--minus"></i>
-        </div>
-      </div>
+      <ZoomControl />
       <div className="map-menu__group">
         <div className="map-menu__item map-menu__link">
           <i className="gis-icon gis-icon--eraser-thin icon-lg"></i>
@@ -45,9 +29,7 @@ const MapMenu: React.FC = () => {
           <i className="gis-icon gis-icon--layers-remove-thin icon-lg"></i>
         </div>
       </div>
-      <div className="map-menu__group">
-        <div className="map-menu__item">x,y 221996,632828</div>
-      </div>
+      <XYControl />
     </div>
   );
 };
