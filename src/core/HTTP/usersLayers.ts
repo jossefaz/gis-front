eba56c5b85ade2d27c1d16402be71b7d56d3117a
jossefaz <jossefaz@protonmail.com>
2021-04-18 21:11:43 +0300

    add http client for users layers and geofile

---
+++ src/core/HTTP/usersLayers.ts
@@ -0,0 +1,19 @@
+import { TokenData, UserCredentials } from "../types";
+import HTTPFactory from "./HTTPFactory";
+import config from "./../../configuration";
+import { ApiCall } from "../types/http";
+
+export async function createLayers(formData: FormData) {
+  const client = HTTPFactory.getInstance(config().API["geofiles"]);
+  const request: ApiCall = {
+    url: "/",
+    method: "POST",
+    headers: { "Content-Type": "multipart/form-data" },
+    data: formData,
+  };
+  const { data, status } = await client.request<TokenData>(request);
+  if (status === 200 && "access_token" in data) {
+    return data;
+  }
+  return false;
+}
