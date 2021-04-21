d1be196f04599b77ce63a2165673ac41d24d5e4e
jossefaz <jossefaz@protonmail.com>
2021-04-21 11:10:48 +0300

    add Geofiles

--- src/components/tool/InternalTool/Geofiles/index.tsx
+++ src/components/tool/InternalTool/Geofiles/index.tsx
@@ -1,21 +1,59 @@
-import { FC, useState, useEffect } from "react";
+import React, { FC, useState, useEffect, useMemo, useCallback } from "react";
 import { retrieveAllFiles, GeofileItem } from "../../../../core/HTTP/geofiles";
+import { useDropzone } from "react-dropzone";
+import { acceptStyle, activeStyle, baseStyle, rejectStyle } from "./style";
 import _ from "lodash";
 
 const Geofiles: FC = () => {
   const [filesMetadata, setFilesMetadata] = useState<GeofileItem[]>([]);
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(`acceptedFiles`, acceptedFiles);
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: [
+      "application/zip",
+      "image/x-dwg",
+      "image/x-dxf",
+      "drawing/x-dwf",
+      "application/json",
+    ],
+  });
 
   useEffect(() => {
     retrieveAllFiles().then((fetchedFiles) => {
       const metadataChanged = _.isEqual(filesMetadata, fetchedFiles);
       if (!metadataChanged && fetchedFiles) {
-        console.log(`updated`, fetchedFiles);
         setFilesMetadata(fetchedFiles);
       }
     });
   });
 
-  return <div></div>;
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
+
+  return (
+    <div className="container">
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>Choose geographic files</p>
+      </div>
+    </div>
+  );
 };
 
 export default Geofiles;
